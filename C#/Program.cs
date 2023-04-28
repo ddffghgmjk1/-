6a8a57b1461b3875340d7adb7b5de9e8a7a3e4ce
ddffghgmjk1 <132074262+ddffghgmjk1@users.noreply.github.com>
2023-04-28 17:43:08 +0300

    Add files via upload

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,10 +1,12 @@
-﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-//4 -> да
-//-3 -> нет
-//7 -> нет
-
-System.Console.WriteLine("Введите первое число");
-int a = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine(a/2);
-Console.WriteLine(a%2);
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+System.Console.WriteLine("Ведите число");
+int numbers = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i< numbers+1; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write(i+"");
+    }
+}
